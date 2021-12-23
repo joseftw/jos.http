d1f342da06f71ab62036850390603e29fa73fd00
joseftw <joseftw@localhost.local>
2021-12-23 17:26:41 +0100

    Cleanup

---
+++ test/JOS.Http.Tests/DummyHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace JOS.Http.Tests;
+
+public class DummyHttpClient
+{
+    private readonly HttpClient _httpClient;
+
+    public DummyHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public async Task<string> GetData()
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, "/data");
+
+        using var response = await _httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
